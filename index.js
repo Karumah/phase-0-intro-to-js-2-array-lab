2d50799eeae765edc19016c11cc3f1093a6b59e4
Trevor Karuma <trevormukundi@gmail.com>
2025-03-12 00:42:00 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo","Otis","Garfield"]
+
+function  destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+}
+
+function appendCat(name) {
+    return [...cats, 'Broom'];
+}
+
+function prependCat(name) {
+    return [ 'Arnold' , ...cats];
+}
+
+function  removeLastCat() {
+    return cats.slice(0, -1)
+}
+
+function  removeFirstCat(){
+    console.log(`${cats}`)
+    return cats.slice(1)
+}
